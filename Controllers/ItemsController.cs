d55285b7b4a82deb74d6bafc707605bd9ddcc7b7
Guest User <Guest@epicodus-39.local>
2018-02-12 10:44:54 -0800

    Adds categories to to-do-list.

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -13,23 +13,22 @@ namespace ToDoList.Controllers
             List<Item> allItems = Item.GetAll();
             return View(allItems);
         }
-        [HttpGet("/items/new")]
-        public ActionResult CreateForm()
+        [HttpGet("/categories/{categoryId}/items/new")]
+        public ActionResult CreateForm(int categoryId)
         {
-            return View();
+           Dictionary<string, object> model = new Dictionary<string, object>();
+           Category category = Category.Find(categoryId);
+           return View(category);
         }
-        [HttpPost("/items")]
-        public ActionResult Create()
+        [HttpGet("/categories/{categoryId}/items/{itemId}")]
+        public ActionResult Details(int categoryId, int itemId)
         {
-          Item newItem = new Item (Request.Form["new-item"]);
-          List<Item> allItems = Item.GetAll();
-          return View("Index", allItems);
-        }
-        [HttpGet("/items/{id}")]
-        public ActionResult Details(int id)
-        {
-          Item item = Item.Find(id);
-          return View(item);
+           Item item = Item.Find(itemId);
+           Dictionary<string, object> model = new Dictionary<string, object>();
+           Category category = Category.Find(categoryId);
+           model.Add("item", item);
+           model.Add("category", category);
+           return View(item);
         }
       }
     }
